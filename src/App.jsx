77531a24198d3dab66d7b5e32237a01fed9b53fa
courtraee <courtney.rae@hotmail.co.uk>
2024-04-22 18:07:03 +0100

    Set up components and layout within app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import 'react'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Intro from "./components/intro/intro";
+import Projects from "./components/projects/projects";
+import Connect from "./components/connect/connect";
+import Footer from "./components/footer/footer";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+
+      <section className="hero">
+        <Hero />
+      </section>
+
+      <section className="intro">
+        <Intro />
+      </section>
+
+      <section className="projects">
+        <Projects />
+      </section>
+
+      <section className="connect">
+        <Connect />
+      </section>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
