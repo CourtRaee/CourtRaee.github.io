d0288da305c1010d6a75d13255436d13ec65fcf1
courtraee <courtney.rae@hotmail.co.uk>
2024-04-23 21:25:09 +0100

    Added tech stack components, not including styling

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -2,7 +2,8 @@ import "react";
 import "./intro.css";
 import IPhoneMockup from "../iPhoneMockup/iPhoneMockup";
 import WelcomeSection from "../welcomeSection/welcomeSection";
-import Bitmoji from "../bitmoji/bitmoji"
+import Bitmoji from "../bitmoji/bitmoji";
+import TechStackCard from "../techStackCard/techStackCard";
 
 function Intro() {
   return (
@@ -12,7 +13,6 @@ function Intro() {
       </div>
 
       <div className="intro-container">
-
         <div className="intro-one">
           <div className="welcome-container">
             <WelcomeSection />
@@ -25,10 +25,30 @@ function Intro() {
 
         <div className="intro-two">
           <div className="tech-stack-container">
-            <p>test</p>
+            <h1 className="tech-stack-title">my tech stack.</h1>
+            <div className="cards-container">
+              <TechStackCard 
+                image={"/src/assets/html.png"}
+                text={"html"}
+              />
+
+              <TechStackCard 
+                image={"/src/assets/css.png"}
+                text={"css"}
+              />
+
+              <TechStackCard 
+                image={"/src/assets/javascript.png"}
+                text={"javascript"}
+              />
+
+              <TechStackCard 
+                image={"/src/assets/react.png"}
+                text={"react"}
+              />
+            </div>
           </div>
         </div>
-
       </div>
     </>
   );
--- src/components/techStackCard/techStackCard.jsx
+++ src/components/techStackCard/techStackCard.jsx
@@ -1,8 +1,13 @@
 import "react";
 import "./techStackCard.css";
 
-function TechStackCard() {
-  return;
+function TechStackCard( {image, text} ) {
+  return (
+    <div className="tech-stack-card">
+      <img src={image}></img>
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default TechStackCard;
