c1c1b555f9e7ffdb032b44a59db605b510334027
courtraee <courtney.rae@hotmail.co.uk>
2024-04-23 19:13:36 +0100

    Set up intro containers

--- src/components/iPhoneMockup/iPhoneMockup.jsx
+++ src/components/iPhoneMockup/iPhoneMockup.jsx
@@ -1,8 +1,12 @@
 import "react";
 import "./iPhoneMockup.css";
 
+import tempiPhone from "../../assets/iphone-temp-mockup.png";
+
 function IPhoneMockup() {
-  return;
+  return (
+      <img src={tempiPhone} className="temp-iPhone-mockup" />
+  );
 }
 
 export default IPhoneMockup;
--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,8 +1,25 @@
 import "react";
 import "./intro.css";
+import IPhoneMockup from "../iPhoneMockup/iPhoneMockup";
 
 function Intro() {
-  return;
+  return (
+    <>
+      <div className="iphone-container">
+        <IPhoneMockup />
+      </div>
+
+      <div className="intro-container">
+        <div className="welcome-container">
+          <p>testing</p>
+        </div>
+
+        <div className="tech-stack-container">
+          <p>test</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Intro;
