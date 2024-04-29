dadade447b91c0708d60db9081220cc888d9cd04
courtraee <courtney.rae@hotmail.co.uk>
2024-04-29 16:21:39 +0100

    Finished building intro section

--- src/components/techStackCard/techStackCard.jsx
+++ src/components/techStackCard/techStackCard.jsx
@@ -5,7 +5,7 @@ function TechStackCard( {image, text} ) {
   return (
     <div className="tech-stack-card">
       <img src={image}></img>
-      <p>{text}</p>
+      <p className="tech-stack-card-text">{text}</p>
     </div>
   );
 }
