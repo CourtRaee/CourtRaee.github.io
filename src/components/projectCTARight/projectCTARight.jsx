23a6de783e65f6a4114628b364afe0eac1f2f451
courtraee <courtney.rae@hotmail.co.uk>
2024-04-29 19:31:58 +0100

    Completed project set up

--- src/components/projectCTARight/projectCTARight.jsx
+++ src/components/projectCTARight/projectCTARight.jsx
@@ -1,11 +1,11 @@
 import "react";
 import "./projectCTARight.css";
 
-function ProjectCTARight( { text }) {
+function ProjectCTARight({ text }) {
   return (
     <div className="right-cta-container">
       <h1 className="project-title-right">{text}</h1>
-      <button className="project-button">take me there ➡️</button>
+      <button className="project-button">⬅️ show me</button>
     </div>
   );
 }
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -17,7 +17,21 @@ function Projects() {
 
       <div className="project-two-container">
         <ProjectCTALeft 
+          text={"serveasy wireframes."}
+        />
+        <ProjectPreview />
+      </div>
+
+      <div className="project-three-container">
+        <ProjectPreview />
+        <ProjectCTARight 
           text={"dialled wireframes."}
+        />
+      </div>
+
+      <div className="project-four-container">
+        <ProjectCTALeft 
+          text={"soc progression."}
         />
         <ProjectPreview />
       </div>
