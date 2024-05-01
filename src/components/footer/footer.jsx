563352f9b7a5094d2808da525c03af58ecd24723
courtraee <courtney.rae@hotmail.co.uk>
2024-05-01 18:18:23 +0100

    Created footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,9 @@ import "react";
 import "./footer.css";
 
 function Footer() {
-  return;
+  return (
+    <p className="created-by-courtney-text">Created by Courtney Smith.</p>
+  );
 }
 
 export default Footer;
