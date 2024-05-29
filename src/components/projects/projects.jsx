037637e636f4355faed652b977e9e785c175d994
Courtney Smith <Court@a1226.home>
2024-05-29 14:53:32 +0100

    Rearrange layout

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -13,24 +13,6 @@ function Projects() {
     <div className="projects-container">
       <h1 className="project-text">my projects.</h1>
       <div className="project-one-container">
-        <ProjectPreview image={f1Preview} />
-        <ProjectCTA
-          text={"f1 driver results."}
-          buttonText="take me there ➡️"
-          link="/f1"
-        />
-      </div>
-
-      <div className="project-two-container">
-        <ProjectCTA
-          text={"serveasy wireframes."}
-          buttonText="⬅️ show me"
-          link="/serveasy"
-        />
-        <ProjectPreview image={serveasyWireframes} />
-      </div>
-
-      <div className="project-three-container">
         <ProjectPreview image={dialledCard} />
         <ProjectCTA
           text={"dialled wireframes."}
@@ -39,13 +21,31 @@ function Projects() {
         />
       </div>
 
-      <div className="project-four-container">
+      <div className="project-two-container">
         <ProjectCTA
           text={"soc progression."}
           buttonText="⬅️ let's see"
           link="/soc"
         />
         <ProjectPreview image={socCard} />
+      </div>
+
+      <div className="project-three-container">
+        <ProjectPreview image={f1Preview} />
+        <ProjectCTA
+          text={"f1 driver results."}
+          buttonText="take me there ➡️"
+          link="/f1"
+        />
+      </div>
+
+      <div className="project-four-container">
+        <ProjectCTA
+          text={"serveasy wireframes."}
+          buttonText="⬅️ show me"
+          link="/serveasy"
+        />
+        <ProjectPreview image={serveasyWireframes} />
       </div>
     </div>
   );
