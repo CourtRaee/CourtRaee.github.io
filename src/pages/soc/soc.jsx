2e24965628333eb31fbd196ca88aba6218fe316f
courtraee <courtney.rae@hotmail.co.uk>
2024-06-19 13:39:21 +0100

    Added user experience content

--- src/pages/soc/soc.jsx
+++ src/pages/soc/soc.jsx
@@ -61,6 +61,64 @@ function Soc() {
           <source src="/assets/demo-video.mov" type="video/mp4"></source>
         </video>
       </div>
+
+      <div className="ux-container">
+        <h1 className="ux-header">Considering the User Experience</h1>
+        <p className="ux-copy">
+          When meeting with our stakeholder, we explored her main pain points
+          with her current software and carefully asked in-depth questions that
+          would help us better understand what exactly it was that she was
+          envisioning. <br></br>
+          <br></br>We took the time to understand our stakeholder more in-depth
+          and how she operates. We learnt that she is someone who doesn’t like
+          to search around for a solution to a problem. If she doesn’t know
+          something, she typically wouldn’t turn to Google to find the answer.{" "}
+          <br></br>
+          <br></br>Because of this, we concluded that her main need was ease and
+          speed with this new software, as there was a lot of searching and
+          manoeuvring to get all the information she required on her old one.
+          <br></br>
+          <br></br>
+          With this in mind, we wanted to create a solution that would be in an
+          easy-to-read and understandable format. <br></br>
+          <br></br>We decided to create 3 calendar views that would easily show
+          all the information she was lacking with her current software, as
+          shown in the wireframe.
+        </p>
+        <img src="/assets/year-wireframe.png" className="wireframes"></img>
+        <p className="ux-copy">
+          Due to the volume of information that would be present, we were
+          mindful of the calendar looking too busy but still also getting what
+          she needed across clearly.<br></br>
+          <br></br> We decided to use colour-coded bars to represent the
+          departments to ensure speed and ease which is a key part of solving
+          our stakeholders problem.
+        </p>
+        <img src="/assets/six-month-wireframe.png" className="wireframes"></img>
+        <p className="ux-copy">
+          Our stakeholder also needed to know what type of communications would
+          be going out at a glance, therefore we decided to use emojis.<br></br>
+          <br></br> We chose emojis over icons as there is generally more
+          familiarity with them and they’re more accessible.<br></br>
+          <br></br> Our research concluded that we as humans process visuals
+          60,000 times faster than text, therefore using visuals such as emojis
+          was key for the user’s needs.
+        </p>
+        <img src="/assets/month-wireframe.png" className="wireframes"></img>
+        <p className="ux-copy">
+          We built a prototype that we shared with our stakeholder so she could
+          get the full effect of what the product would look and feel like.
+          <br></br>
+          <br></br> We focused on small, cheap iterations and feedback from our
+          stakeholder to make sure we solved the actual problem in a way that
+          was useful in practice.<br></br>
+          <br></br> Based on the feedback we received after we created the
+          prototype, we made small tweaks such as switching from a 6 month view
+          to a quarterly view to help better tailor the product to our
+          stakeholders needs.
+        </p>
+        <img src="/assets/demo.png" className="wireframes"></img>
+      </div>
       <Footer />
     </BodyContainer>
   );
