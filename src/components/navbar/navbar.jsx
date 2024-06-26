6e01cadfbaf0a68f3ec3e6a3a706c77067faab19
Court <108122747+CourtRaee@users.noreply.github.com>
2024-06-26 10:56:35 +0100

    Tweak nav links

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
   return (
     <section className="navbar">
       <div className="projects-link">
-        <a href="#projects">
+        <a href="/#projects">
           <button className="navbar-projects-button">projects.</button>
         </a>
-        <a href="#connect">
+        <a href="/#connect">
           <button className="navbar-contact-button">speak to me.</button>
         </a>
       </div>
